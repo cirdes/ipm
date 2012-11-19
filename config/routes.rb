716c4c779a1daf551016fc8f7b0087bdeb5a91b8
Cirdes Henrique <cirdes@codemedia.com.br>
2012-11-19 01:20:42 -0300

    remove slider views

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Ipm::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  resources :sliders
 
-  root :to => 'high_voltage/pages#show', :id => 'index'
+  resources :pages, :controller => 'pages', :only => [:show]
 
-  match "/pages/*id" => 'pages#show', :as => :page, :format => false
+  root :to => 'pages#show', :id => 'index'
+
+  #match "/pages/*id" => 'pages#show', :as => :page, :format => false
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
